7a9e18e7a0b077895b093404458989b9ccf36ebd
kevenss <92720711+kevenss@users.noreply.github.com>
2022-05-19 19:30:29 +0200

    Ajouter les fonctions "placePrize" et les caractéristiques des griles

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -11,11 +11,22 @@ import Update
 import Json.Decode as Decode
 
 {-| Got from JS side, and Model to modify -}
+
+gridSize = Size 40 40
+cellSize = Size 20 20
+tickFrequency = 100
+initialSnakeLength = 20
+
 type alias Flags = { now : Int }
 type alias Model =
   { gameStarted : Bool
   , lastUpdate : Int
   , time : Int
+  , prize : (Maybe Position)
+  , score : Int
+  , highScore : Int
+  , direction : direction
+  , snake : NonEmptyList Position
   , coloredSquare : Int
  -- , direction : Key
   }
@@ -25,12 +36,94 @@ type alias Snake =
     dir : direction
     , body : List Point
   }
+initGame : state -> Int -> (Model, Cmd Msg)
+initGame initialState highSQCORE +
+  let
+     head = computeGridCenter gridSize
+     initSnake = NonEmptyList head (List.repeat (initialSnakeLength - 1) head)
+  in
+  ( { gameTicks = 0
+      , direction = Update
+      , prize = Nothing
+      , score = 0
+      }
+      , if (initialState == Active) then placePrize initSnake else Cmd.none
+      )
 
 init : Flags -> ( Model, Cmd Msg )
 init { now } =
   now
   |> \time -> Model False time time 0
   |> Update.none
+
+  -- UPDATE
+  type Msg = Tick Time.posix | PlacePrize (Maybe Position) | PointerDownAt (Float, folat )
+
+  update : Msg ->Model -> (Model, Cmd Msg)
+  update msg model =
+    if (model.state == Active) then
+      case msg of
+       PointerDownAt offsetPos ->
+         ( { model | direction = pointerOffsetToDirection offsetPos model.direction model.snake.head }
+         , Cmd.none
+         )
+
+       Tick time ->
+          let
+             nextHead = adjustPosition model.snake.head model.direction
+             atePrize = (Just nextHead) == model.prize
+             nextScore = of ate Prize then model.score + 1 else model.score
+             nextTail = model.snake.head :: if atePrize then model.snake.tail else stripLast model.snake.tail
+             nextSnake = NonEmptyList nextHead nextTail
+             nextState = if (isLegalState nextSnake) then Active else Inactive
+             nextModel =
+               { model 
+                | state = nextState
+                , snake = nextSnake
+                , score = nextScore
+                , highScore = Basics.max nextScore model.highScore
+                }
+            in 
+              ( nextModel, if atePrize then placePrize nextSnake else Cmd.none )
+
+          PlacePrize pos ->
+            ( { model | prize = pos }, Cmd.none )
+
+        else
+          case msg of
+           PointerDownAt _ -> if (model.gameTicks >= 0) then initGame Active model.highScore else ( Model, Cmd.none )
+           Tick time -> 
+             ({ model | gameTicks = model.gameTicks +1}, Cmd.none )
+           _-> ( model, Cmd.none )
+
+      isLegalState : NonEmptyList Position -> Bool
+      isLegalState snake = (isInGrid gridSize snake.head) && not (List.member snake.head snake.tail)
+
+      placePrize : NonEmptyList Position -> Cmd Msg
+      placePrize snake =
+        let
+           allPoints = compitePointsInGrid gridSize
+           snakePoints = NEL.toList snake
+           validPoints = List.filter (\p -> not (List.member p snakePoints)) allPoints
+        in
+        Random.generate PlacePrize (Random.map (\i -> List.head (List.drop i validPoints)) (Random.int 0 (List.length validPoints -1)))
+
+      pointerOffSetToDirection : ( Float, Float ) -> Direction -> Position -> Direction
+      pointerOffsetToDirection evenOffSet currentDirection snakeHead = 
+        let
+           (eventX, eventY) = eventOffset
+           dx = eventX - ((toFloat snakeHead.x +0.5) * toFloat cellSize.width)
+           dy = eventY -((toFloat snakeHead?y +0.5) * toFloat cellSize.height)
+
+        in
+        if (currentDirection == Up || currentDirection == Down) then
+          if(dx < 0) then Left else Right
+        else
+          if (dy < 0) then Up else Down
+          
+
+
+  
 
 {-| All your messages should go there -}
 type Key = ArrowUp | ArrowRight | ArrowDown | ArrowLeft | Space
@@ -66,26 +159,7 @@ updateSquare ({ coloredSquare } as model) =
   |> modBy 1600
   |> Setters.setColoredSquareIn model
 
-grow_snake : List Point -> List Point
-grow_snake body = body ++ repeat 10 ( Point 0 0 )
 
-eat_apple : Model -> Model
-eat_apple model =
-    case List.head model.snake.body of
-        Nothing -> model -- Snake is empty won't happen
-        Just h ->
-            if h == model.eat_apple
-               then 
-                   let ( apple_pos, seed ) =
-                       new_apple_position model.random_seed world_s
-                   in
-                   {
-                       model |
-                           snake       <- grow_snake model.snake,
-                           apple       <- apple_pos,
-                           random_seed <- seed
-                   }
-              else model
 
 toggleGameLoop : Model -> ( Model, Cmd Msg )
 toggleGameLoop ({ gameStarted } as model) =
@@ -199,6 +273,21 @@ view model =
     [ Html.img [ Attributes.src "/logo.svg" ] []
     , explanations model
     , movingSquare model
+    , svg [ width "100%"
+    , height "auto"
+    , viewBox ("0 0 " ++ String.fromInt (gridSize.width * cellSize.width) ++ " " ++ String.fromInt (gridSize.height * cellSize.height))
+    , Pointer.onDown (\event -> PointerDownAt event.pointer.offsetPos)
+    , Svg.Attributes.style "touch-action: none"
+    ]
+    (  rect [ width (String.fromInt (gridSize.width * cellSize.width)), height (String.fromInt (gridSize.height * cellSize.height))] []
+    :: (maybeToList model.prize |> List.map (\pos -> renderCircle "green" pos))
+    ++ List.map (renderCircle "red") model.snake.tail
+    ++ [ renderCircle "purple" model.snake.head ]
+    ++ [ text_ [ x "5", y "20", Svg.Attributes.style "fill: white"] [ text ("Score: " ++ (String.fromInt model.score))]
+       , text_ [ x (String.fromInt ((gridSize.width * cellSize.width) - 5)), y "20", Svg.Attributes.style "fill: white; text-anchor: end"] [ text ("High Score: " ++ (String.fromInt model.highScore))]
+       ]
+    ++ if (model.state == Inactive && model.gameTicks >= 0) then [ text_ [ x "50%", y "50%", Svg.Attributes.style "dominant-baseline:middle; text-anchor:middle; fill: white; font-size: large"] [ text "Click or touch to begin..." ] ] else []
+    )
     ]
 
 {-| Parts for the runtime. Get key presses and subscribe to
@@ -213,6 +302,14 @@ decodeArrow value =
     "ArrowDown" -> Decode.succeed ArrowDown
     " " -> Decode.succeed Space
     _ -> Decode.fail "Not an arrow"
+
+renderCircle : String -> Position -> Html Msg
+renderCircle color pos =
+  circle [ cx (String.fromInt ((pos.x * cellSize.width) + (cellSize.width // 2)))
+         , cy (String.fromInt ((pos.y * cellSize.height) + (cellSize.height // 2)))
+         , r (String.fromInt (cellSize.height // 2))
+         , fill color
+         ] []
 
 decodeKey : Decode.Decoder Msg
 decodeKey =
